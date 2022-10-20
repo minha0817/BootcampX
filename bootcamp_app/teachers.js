9b4629505689375179c97ece243e2a3f35b70684
minha0817 <minhakk24@gmail.com>
2022-10-20 12:23:55 -0700

    make it Parameterized Query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,26 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const query = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+pool
+  .query(
+    `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = $1
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`;
-
-pool.query(query, [process.argv[2]]).then((res) => {
-  res.rows.forEach((user) => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  });
-});
+`,
+    values
+  )
+  .then((res) => {
+    console.log("res", res.rows);
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
